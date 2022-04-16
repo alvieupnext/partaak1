7109d0e0f00bba666d443b2dcda18f49443a2302
Alvaro Vargas <Alvaro.Javier.Vargas.Guerrero@vub.be>
2022-04-16 12:02:13 +0200

    Removed unwanted stuff

--- firstAttempt (useless)/pcscript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-lscpu > cpuspecs.txt
-java -jar benchmarks.jar pc.phaseOne.seq -rf csv -rff seq.csv
-java -jar benchmarks.jar pc.phaseOne.cores -rf csv -rff cores.csv
-java -jar benchmarks.jar pc.phaseOne.threshold -rf csv -rff threshold.csv
